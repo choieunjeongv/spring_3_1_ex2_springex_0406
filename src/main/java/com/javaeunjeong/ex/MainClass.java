23a7ef741e6e2a1ce0a3bf3a81ad702bf6a7b388
choieunheong <mjchoi17@hanmail.net>
2022-04-13 10:59:04 +0900

    3-1 di 최초 예제

---
+++ src/main/java/com/javaeunjeong/ex/MainClass.java
@@ -0,0 +1,33 @@
+package com.javaeunjeong.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		MyCalculator myCalculator= new MyCalculator();
+//		myCalculator.setCalculator(new Calculator());
+//		
+//		myCalculator.setFirstNum(10);
+//		myCalculator.setSecondNum(20);
+//		
+//		myCalculator.add();
+//		myCalculator.sub();
+//		myCalculator.mul();
+//		myCalculator.div();
+		
+		String configLocation = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		MyCalculator myCalculator = ctx.getBean("mycalculator", MyCalculator.class);
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+		
+		ctx.close();
+	}
+
+}
---
+++ src/main/java/com/javaeunjeong/ex/MyCalculator.java
@@ -0,0 +1,53 @@
+package com.javaeunjeong.ex;
+
+public class MyCalculator {
+	
+	Calculator calculator;//ÀÇÁ¸ °´Ã¼
+	private int firstNum;
+	private int secondNum;
+	
+	
+	
+	public MyCalculator() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void add() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void sub() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void mul() {
+		calculator.addition(firstNum, secondNum);
+	}
+	public void div() {
+		calculator.addition(firstNum, secondNum);
+	}
+	
+	
+	
+	
+	public Calculator getCalculator() {
+		return calculator;
+	}
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+	public int getFirstNum() {
+		return firstNum;
+	}
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public int getSecondNum() {
+		return secondNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+	
+}
